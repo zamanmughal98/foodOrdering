4c88ecd385bfd35a19195f8973342189ab73d223
Zaman Mughal <zamanmughal98@gmail.com>
2022-01-10 16:12:18 +0500

    branch2

--- src/testingBranch 2/testingBranch2.tsx
+++ src/testingBranch 2/testingBranch2.tsx
@@ -4,8 +4,6 @@ const testingBranch2 = ()=> {
     return(
         <div>
             <h1>Testing Branch2</h1>
-            <h2>Testing Branch2</h2>
-            <p>Testing Branch2</p>
         </div>
     );
 };
