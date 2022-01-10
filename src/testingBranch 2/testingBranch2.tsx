afb7c4e12b2a5c2c81705013a415f2abb8384a86
Zaman Mughal <zamanmughal98@gmail.com>
2022-01-10 15:53:14 +0500

    testing branch 2 created

--- src/testingBranch 2/testingBranch2.tsx
+++ src/testingBranch 2/testingBranch2.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const testingBranch2 = ()=> {
     return(
         <div>
-            <h1>Testing ranch2</h1>
-            <h2>Testing ranch2</h2>
-            <p>Testing ranch2</p>
+            <h1>Testing Branch2</h1>
+            <h2>Testing Branch2</h2>
+            <p>Testing Branch2</p>
         </div>
     );
 };
