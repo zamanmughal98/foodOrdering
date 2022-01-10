01cef326af8ad6a2216c398b402b0fb16835093a
Zaman Mughal <zamanmughal98@gmail.com>
2022-01-10 12:47:30 +0500

    testing Branch

---
+++ src/testingBranch/testingBranch.tsx
@@ -0,0 +1,11 @@
+/*eslint linebreak-style: ["error", "windows"]*/
+
+import React from "react";
+const testingBranch = ()=> {
+    return(
+        <div>
+            <h1> This is Testing Branch ....</h1>
+        </div>
+    );
+};
+export default testingBranch;
