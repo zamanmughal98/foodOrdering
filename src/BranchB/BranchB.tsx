6d462fcba1f1245254d86d3a3f475611851e2879
Zaman Mughal <zamanmughal98@gmail.com>
2022-01-10 23:53:58 +0500

    branchB new

---
+++ src/BranchB/BranchB.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+import React from "react";
+
+const BranchB = () => { 
+    return (
+        <div>
+            <h1>Branch B</h1>
+        </div>
+    );
+};
+export default BranchB;
