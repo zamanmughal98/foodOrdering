1df51ef1280ea2e6f6ba166abe08acd79eb4a629
Zaman Mughal <zamanmughal98@gmail.com>
2022-01-10 14:44:13 +0500

    removed 2 Heading tags

--- src/testingBranch/testingBranch.tsx
+++ src/testingBranch/testingBranch.tsx
@@ -5,9 +5,6 @@ const testingBranch = ()=> {
     return(
         <div>
             <h1> This is Testing Branch ....</h1>
-            <h1> This is Testing Branch ....</h1>
-            <h1> This is Testing Branch ....</h1>
-
         </div>
     );
 };
