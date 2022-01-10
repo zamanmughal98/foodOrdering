2fbc4353db36a94d2a6d0dad68da86e11e24f43a
Zaman Mughal <zamanmughal98@gmail.com>
2022-01-10 15:01:50 +0500

    One more Heading added

--- src/testingBranch/testingBranch.tsx
+++ src/testingBranch/testingBranch.tsx
@@ -5,6 +5,8 @@ const testingBranch = ()=> {
         <div>
             <section>
                 <h1>Testing Branch Heading one....</h1>
+                <h1>Testing Branch Heading Two....</h1>
+
             </section>
         </div>
     );
