e073f9facd4adc6ba1041759d0fe0ae5dea018dc
Zaman Mughal <zamanmughal98@gmail.com>
2022-01-10 15:33:16 +0500

    section removed

--- src/testingBranch/testingBranch.tsx
+++ src/testingBranch/testingBranch.tsx
@@ -3,10 +3,7 @@ import React from "react";
 const testingBranch = ()=> {
     return( 
         <div>
-            <section>
-                <h1>Testing Branch Heading one....</h1>
-                <h1>Testing Branch Heading Two....</h1>
-            </section>
+            <h1>Testing Branch Heading one....</h1>
         </div>
     );
 };
