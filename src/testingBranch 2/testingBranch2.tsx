09a760ea5389ecfa8c68f6179cfbcdac3700dd51
Zaman Mughal <zamanmughal98@gmail.com>
2022-01-10 15:51:56 +0500

    testing branch 2 created

---
+++ src/testingBranch 2/testingBranch2.tsx
@@ -0,0 +1,12 @@
+/*eslint linebreak-style: ["error", "windows"]*/
+import React from "react";
+const testingBranch2 = ()=> {
+    return(
+        <div>
+            <h1>Testing ranch2</h1>
+            <h2>Testing ranch2</h2>
+            <p>Testing ranch2</p>
+        </div>
+    );
+};
+export default testingBranch2;
